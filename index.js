b54bb740f8b4f098cda5430c8d263d7247fd7349
Alissar Arnaout <Alissar@Alissars-MacBook-Air.local>
2020-03-27 14:25:18 -0400

    cleaned up code + debug connections

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 let app = require('express')();
+
 let http = require('http').Server(app);
+
 let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-// connect to local host
-http.listen(3000, () => {
-    console.log('connected')
-})
 
 // listen for socket connection
-io.on('connection', (socket) => {
+io.on('connections', (socket) => {
     io.emit('connections',Object.keys(io.sockets.connected).length) // number of connections
 
     socket.on('disconnect', () => {
-        console.log('Disconnected');
+        console.log('Disconnected')
+        io.emit('connections', Object.keys(io.sockets.connected).length);
     })
 
     // listen for created events
-    socket.on('Created',(data) => {
-        socket.broadcast.emit('Created', (data))
+    socket.on('chat-message',(msg) => {
+        socket.broadcast.emit('chat-message', msg)
     })
 
     // user is typing
     socket.on('typing',(data) => {
-        socket.broadcast.emit('typing',(data))
+        socket.broadcast.emit('typing', data)
     })
 
     // user has stopped typing
-    socket.on('stopTyping',(data) => {
-        socket.broadcast.emit('stopTyping',(data))
+    socket.on('stopTyping', () => {
+        socket.broadcast.emit('stopTyping')
     })
 
     // user joined the chat / connected
-    socket.on('joined', (data) => {
-        socket.broadcast.emit('joined',(data))
+    socket.on('joined', (name) => {
+        socket.broadcast.emit('joined', name)
     })
 
     // user has left the chat / disconnected
-    socket.on('left', (data) => {
-        socket.broadcast.emit('left',(data))
+    socket.on('left', (name) => {
+        socket.broadcast.emit('left', name)
     })
 
+})
 
+http.listen(3000, () => {
+    console.log('server started at http://localhost:3000')
 })
